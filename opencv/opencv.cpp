1fa3cf45b2573677380d35a2e7c43f9e732a1743
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2021-05-31 15:05:15 +0300

    added fisheye lens
    add fisheye lens projection + panoramic

--- opencv/opencv.cpp
+++ opencv/opencv.cpp
@@ -3,28 +3,82 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <math.h> 
 
 using namespace std;
 using namespace cv;
 
 const double PI = 3.141592653589793;
-const string PATH_IMAGE = "C:/Users/georg/OneDrive/Desktop/img2.png";
+const string PATH_IMAGE = "C:/Users/georg/OneDrive/Desktop/img11.png";
 const int ESC = 27;
 
 //Find the corresponding fisheye outpout point corresponding to an input cartesian point
-Point2f findFisheye(int Xe, int Ye, double R, double Cfx, double Cfy, double He, double We) {
-    Point2f fisheyePoint;
-    double theta, r, Xf, Yf; //Polar coordinates
+Point2f findFisheye(int Xe, int Ye, double R, double Cfx, double Cfy, double He, double We,int Hf, int Wf) {
+   
+    Point2f pfish;
+       float theta, phi, r;
+       Point3f psph;
+    //
+       float FOV = 3.141592654; // FOV of the fisheye, eg: 180 degrees  
+       float width = Wf;
+       float height = Hf;
+    //
+    //    // Polar angles  
+       theta = 2.0 * 3.14159265 * (Xe / width - 0.5); // -pi to pi  
+       phi = 3.14159265 * (Ye / height - 0.5);  // -pi/2 to pi/2  
+    //
+    //    // Vector in 3D space  
+        psph.x = cos(phi) * sin(theta);
+        psph.y = cos(phi) * cos(theta);
+        psph.z = sin(phi);
+    //
+    //    // Calculate fisheye angle and radius  
+        theta = atan2(psph.z, psph.x);
+        phi = atan2(sqrt(psph.x * psph.x + psph.z * psph.z), psph.y);
+        r = width * phi / FOV;
+    //
+    //    // Pixel in fisheye space  
+        pfish.x = 0.5 * width + r * cos(theta);
+        pfish.y = 0.5 * width + r * sin(theta);
 
-    r = Ye / He * R;
-    theta = Xe / We * 2.0 * PI;
-    Xf = Cfx + r * sin(theta);
-    Yf = Cfy + r * cos(theta);
-    fisheyePoint.x = Xf;
-    fisheyePoint.y = Yf;
+        return pfish;
 
-    return fisheyePoint;
 }
+
+/*Point2f sample(int x, int y) {
+    return Point2f;
+}*/
+
+//Point2f fish2sphere(sampler src)
+//{
+//    vec2 pfish;
+//    float theta, phi, r;
+//    vec3 psph;
+//
+//    float FOV = 3.141592654; // FOV of the fisheye, eg: 180 degrees  
+//    float width = samplerSize(src).x;
+//    float height = samplerSize(src).y;
+//
+//    // Polar angles  
+//    theta = 2.0 * 3.14159265 * (destCoord().x / width - 0.5); // -pi to pi  
+//    phi = 3.14159265 * (destCoord().y / height - 0.5);  // -pi/2 to pi/2  
+//
+//    // Vector in 3D space  
+//    psph.x = cos(phi) * sin(theta);
+//    psph.y = cos(phi) * cos(theta);
+//    psph.z = sin(phi);
+//
+//    // Calculate fisheye angle and radius  
+//    theta = atan(psph.z, psph.x);
+//    phi = atan(sqrt(psph.x * psph.x + psph.z * psph.z), psph.y);
+//    r = width * phi / FOV;
+//
+//    // Pixel in fisheye space  
+//    pfish.x = 0.5 * width + r * cos(theta);
+//    pfish.y = 0.5 * width + r * sin(theta);
+//
+//    return sample(src, pfish);
+//}
 
 int main(int argc, char** argv) {
 
@@ -58,7 +112,7 @@ int main(int argc, char** argv) {
     for (int Xe = 0; Xe < equirectangularImage.size().width; Xe++) {
         for (int Ye = 0; Ye < equirectangularImage.size().height; Ye++) {
 
-            equirectangularImage.at<Vec3b>(Point(Xe, Ye)) = fisheyeImage.at<Vec3b>(findFisheye(Xe, Ye, R, Cfx, Cfy, He, We));
+            equirectangularImage.at<Vec3b>(Point(Xe, Ye)) = fisheyeImage.at<Vec3b>(findFisheye(Xe, Ye, R, Cfx, Cfy, He, We,Hf,Wf));
         }
     }
 
